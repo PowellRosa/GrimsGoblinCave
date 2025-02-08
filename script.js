84307bb2eb71c74fa91bee08d0be876ce0f1e959
PowellRosa <61598103+PowellRosa@users.noreply.github.com>
2025-02-08 13:24:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,25 @@ const audio = document.getElementById('audio');
 
         // Load the first song on page load
         loadSong(currentSongIndex);
+
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        const slides = document.getElementsByClassName("slide");
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1; }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(showSlides, 4000); // Change slide every 4 seconds
+    }
+
+    function plusSlides(n) {
+        slideIndex += n - 1;
+        showSlides();
+    }
+
+
