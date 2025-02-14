caf60e9f748cb58d836c663d12aeb79f4c5d01b7
PowellRosa <61598103+PowellRosa@users.noreply.github.com>
2025-02-13 19:04:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const audio = document.getElementById('audio');
         const songTitle = document.getElementById('song-title');
-        const prevBtn = document.getElementById('prev-btn');
-        const nextBtn = document.getElementById('next-btn');
+        const prevbtn = document.getElementById('prev-btn');
+        const nextbtn = document.getElementById('next-btn');
 
         const songs = [
             { src: 'Music/9am in calabasas (remix) (1).MP3', title: 'Ayesha Erotica-9am in Clabasas (Remix)' },
@@ -26,13 +26,13 @@ const audio = document.getElementById('audio');
         }
 
         // Play the next song
-        nextBtn.addEventListener('click', () => {
+        nextbtn.addEventListener('click', () => {
             currentSongIndex = (currentSongIndex + 1) % songs.length;
             loadSong(currentSongIndex);
         });
 
         // Play the previous song
-        prevBtn.addEventListener('click', () => {
+        prevbtn.addEventListener('click', () => {
             currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
             loadSong(currentSongIndex);
         });
